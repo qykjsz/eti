2ac16eae2ab058a77ba9d605e04da8d8ed52b696
allenwalker-k <aki_kang@163.com>
2019-11-06 17:35:49 +0800

    test

---
+++ ProjectET/AppDelegate/sss.m
@@ -0,0 +1,21 @@
+//
+//  sss.m
+//  ProjectET
+//
+//  Created by hufeng on 2019/11/6.
+//  Copyright © 2019 LightCould. All rights reserved.
+//
+
+#import "sss.h"
+
+@implementation sss
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
