8a7499c6fdd94e49797e197ec118334413432202
349629072@qq.com <349629072@qq.com>
2020-03-05 10:50:53 +0800

    123

--- Pods/Target Support Files/Pods-ProjectET-ProjectETUITests/Pods-ProjectET-ProjectETUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ProjectET_ProjectETUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ProjectET_ProjectETUITests
-@end
--- Pods/Target Support Files/Pods-ProjectETTests/Pods-ProjectETTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ProjectETTests : NSObject
-@end
-@implementation PodsDummy_Pods_ProjectETTests
-@end
